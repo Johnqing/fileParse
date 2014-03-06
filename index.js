be8faf24da25f3042de4d5e04b67701fb5b27ba2
johnqing <csssnow@gmail.com>
2014-03-06 11:36:43 +0800

    add copy dir

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 var fs = require('fs')
 var path = require('path')
+/**
+ * 合并对象
+ * @type {{}}
+ */
+exports.mix = function(receiver, supplier){
+    var args = Array.apply([], arguments),
+        i = 1,
+        key,
+        ride = typeof args[args.length - 1] == "boolean" ? args.pop() : true;
+
+    if(args.length == 1){
+        receiver = ! this.window ? this: {};
+        i = 0;
+    }   
+
+    while ((supplier = args[i])) {
+        i++;
+        for (key in supplier) { //允许对象糅杂，用户保证都是对象
+            if (supplier.hasOwnProperty(key) && (ride || ! (key in receiver))) {
+                receiver[key] = supplier[key];
+            }
+        }
+    }
+    return receiver;
+}
 
 /**
  * 遍历文件夹以及文件
@@ -77,14 +102,43 @@ exports.walk = function() {
         }
     };
 }();
+//创建目录,如果指定路径中有许多中间的目录不存在,也一并创建它们
+/*
+参数
+p为一个目录的路径，以“/”隔开
+ */
+var mkdirSync = exports.mkdirSync = function(p) {
+    p = path.normalize(p);
+    var array = p.split(path.sep); //创建目录,没有则补上
+    for (var i = 0, cur; i < array.length; i++) {
+        if (i === 0) {
+            cur = array[i];
+        } else {
+            cur += (path.sep + array[i]);
+        }
+        try {
+            fs.mkdirSync(cur, "0755");
+        } catch(e) {}
+    }
+}
 /**
  * 文件读取
  * @param file
  * @param success
  */
-exports.readFile = function(file){
+var readFile = exports.readFile = function(file){
     return fs.readFileSync(file, defConf.encode);
 }
+var readFileSync = exports.readFileSync = function() {
+    // Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+    // because the buffer-to-string conversion in `fs.readFileSync()`
+    // translates it to FEFF, the UTF-16 BOM.
+    var content = fs.readFileSync.apply(fs, arguments).toString();
+    if (content.charCodeAt(0) === 0xFEFF) {
+        content = content.slice(1);
+    }
+    return content;
+},
 /**
  * 文件写入
  * @param file
@@ -99,6 +153,43 @@ exports.writeFile = function(file, text){
         });
     });
 }
+//目录对拷,可以跨分区拷贝
+/*
+参数
+old为一个目录的路径，String,以“/”隔开
+neo为一个目录的路径，String,以“/”隔开
+cb 可选，回调    
+ */
+exports.cpdirSync = function(old, neo, cb) {
+    function inner(old, neo) {
+        var array = fs.readdirSync(old);
+        for (var i = 0, n = array.length; i < n; i++) {
+            var source = array[i];
+            var source = path.join(old, source.replace(old, ""));
+            var target = path.join(neo, source.replace(old, ""));
+
+            var stat = fs.statSync(source); //判定旧的IO对象的属性，是目录还是文件或是快捷方式
+            if (stat.isDirectory()) {
+                inner(source, target);
+            } else if (stat.isSymbolicLink()) {
+                fs.symlinkSync(fs.readlinkSync(source), target);
+            } else {
+                fs.writeFileSync(target, readFileSync(source));
+            }
+        }
+    }
+    return function(old, neo, cb) { //把当前目录里面的东西拷贝到新目录下（不存在就创建）
+        old = path.resolve(process.cwd(), old);
+        neo = path.resolve(process.cwd(), neo); //允许拷贝到另一个分区中
+        if (!fs.existsSync(neo)) { //创建新文件
+            mkdirSync(neo);
+        }
+        inner(old, neo);
+        if (typeof cb == "function") {
+            cb();
+        }
+    };
+}()
 /**
  * 获取父级文件夹
  * @param filePath
@@ -120,88 +211,3 @@ exports.isDirIgnore = function(file){
     var dir = getParentDir(file)
     return defConf.ignore.indexOf(dir) != -1
 }
-/**
- * 单个css文件解析
- * @param cssfile
- */
-function oneCssBuild(cssfile){
-    var code = readFile(cssfile);
-    var cssCode = cssParse(code);
-    writeFile(cssfile, cssCode);
-}
-//设置lessc路径
-var lessc = path.resolve('./node_modules/.bin/lessc.cmd');
-/**
- * less文件编译
- * @param filename
- */
-var lessCount;
-var lessCompire = function(filename, fn) {
-    var dir = getParentDir(filename)
-    var lessDir = defConf.workspace + defConf.input + '/' + dir;
-
-    var baseName = path.resolve(lessDir, path.basename(filename, '.less')) + '.css';
-
-    //运行lessc命令
-    exec('' + lessc + ' ' + filename + ' ' + baseName, { encoding: defConf.encode},
-        function (err, stdout, stderr) {
-            if (err != null) {
-                console.log(err)
-            }
-            oneCssBuild(baseName);
-            lessCount--;
-            console.log('剩余：' +lessCount);
-            if(!lessCount){
-                fn && fn();
-            }
-        }
-    );
-};
-
-function lessBuild(fn){
-    var lessPath = defConf.workspace + defConf.lessInput;
-    function build(files){
-        lessCount = files.length;
-        files.forEach(function(file){
-            lessCompire(file, fn);
-        });
-    }
-    walk(lessPath, build);
-}
-
-var TIMES;
-/**
- * 计时
- */
-function onStart(){
-    TIMES = +new Date;
-    console.log('>>Start build');
-}
-
-function onEnd(){
-    var timeUse = +new Date - TIMES;
-    console.log('>>All done: Time use:', timeUse/1000, '秒');
-    console.log('完成时间:', Date());
-}
-/**
- * 多个 css 构建
- */
-
-function cssBuild(){
-    var cssPath = defConf.workspace + defConf.input;
-     function build(files){
-         files.forEach(function(file){
-             if(isDirIgnore(file)) return;
-             oneCssBuild(file);
-         });
-     }
-    walk(cssPath, build);
-}
-/**
- * 开始执行
- */
-(function(){
-    onStart();
-    lessBuild(onEnd);
-//    cssBuild();
-})()
